687f78718161f8a3093ad27592db320df5410475
Myron819 <19036849@rp.edu.sg>
2021-07-22 15:34:21 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/demomusicplayer/MainActivity.java
@@ -0,0 +1,39 @@
+package sg.edu.rp.webservices.demomusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button start, stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // starting the service
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // stopping the service
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+    }
+}
